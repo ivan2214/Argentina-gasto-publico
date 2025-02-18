edbb066aae2b7602a8cd7dd9dc4b925fd9ee6402
ivan2214 <bongiovanniivan12@gmail.com>
2025-02-17 23:40:44 -0300

    style: 🎨 enhance header and navigation
    added background color to header for improved
    visual consistency. updated navigation to
    highlight currently active item and improve
    styling.

--- components/header.tsx
+++ components/header.tsx
@@ -6,7 +6,7 @@ import NavMobile from "./nav-mobile";
 
 export function Header() {
 	return (
-		<header className="sticky top-0 z-50 shadow-sm">
+		<header className="sticky top-0 z-50 bg-background shadow-sm">
 			<Container className="flex items-center justify-between px-4 py-4">
 				<Link href="/" className="flex items-center space-x-2">
 					<span className="font-bold text-primary text-xl">
--- components/nav-desktop.tsx
+++ components/nav-desktop.tsx
@@ -1,12 +1,18 @@
+"use client";
 import Link from "next/link";
 
 import { Button } from "@/components/ui/button";
 import { destacados } from "@/constants";
+import { cn } from "@/lib/utils";
+import { usePathname } from "next/navigation";
 
 const NavDesktop = () => {
+	const pathname = usePathname();
+	console.log(pathname);
+
 	return (
 		<nav
-			className="ml-auto hidden max-w-3xl overflow-x-auto whitespace-nowrap md:flex"
+			className="ml-auto hidden max-w-3xl overflow-x-auto whitespace-nowrap pb-2 md:flex"
 			style={{
 				scrollBehavior: "smooth",
 				scrollbarWidth: "thin",
@@ -21,7 +27,11 @@ const NavDesktop = () => {
 							<Button
 								key={item.title}
 								size="sm"
-								className="transition-colors duration-300 hover:text-primary"
+								className={cn(
+									"transition-colors duration-300 hover:text-primary",
+									pathname === item.link.split("?")[0] &&
+										"bg-primary text-primary-foreground",
+								)}
 								variant="ghost"
 							>
 								<Link href={item.link}>{item.title}</Link>
