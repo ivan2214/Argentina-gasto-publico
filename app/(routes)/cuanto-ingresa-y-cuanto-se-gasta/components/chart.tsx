381d7d52ee4d735e83a20a4ec93fad6f17b2aafe
ivan2214 <bongiovanniivan12@gmail.com>
2025-02-17 23:35:11 -0300

    feat: 🚀 enhance chart with pib data and improved tooltip
    adds pib data to the chart and improves the tooltip
    to display formatted numbers and relevant icons.  the
    tooltip now shows yearly data and uses color-coding
    to indicate whether income exceeds expenses or vice
    versa.  also adds a y-axis for better visualization.

--- app/(routes)/cuanto-ingresa-y-cuanto-se-gasta/components/chart.tsx
+++ app/(routes)/cuanto-ingresa-y-cuanto-se-gasta/components/chart.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import { CartesianGrid, Line, LineChart, XAxis, YAxis } from "recharts";
 
 import {
 	type ChartConfig,
@@ -9,6 +9,9 @@ import {
 	ChartTooltip,
 	ChartTooltipContent,
 } from "@/components/ui/chart";
+import { formatNumber } from "@/lib/utils";
+import type { IngresoEgresoPIB } from "@/types";
+import { BarChart2, Calendar } from "lucide-react";
 
 const chartConfig = {
 	ingreso: {
@@ -19,15 +22,18 @@ const chartConfig = {
 		label: "Gasto",
 		color: "var(--chart-2)",
 	},
+	pib: {
+		label: "PIB",
+		color: "var(--chart-3)", // Nuevo color
+	},
+	ejercicio: {
+		label: "Ejercicio",
+		color: "var(--chart-4)", // Nuevo color
+	},
 } satisfies ChartConfig;
 
 interface ChartProps {
-	data: {
-		ingreso: number;
-		pib: number;
-		gasto: number;
-		ejercicio: number;
-	}[];
+	data: IngresoEgresoPIB[];
 }
 
 export const Chart: React.FC<ChartProps> = ({ data }) => {
@@ -48,8 +54,77 @@ export const Chart: React.FC<ChartProps> = ({ data }) => {
 					axisLine={false}
 					tickMargin={8}
 					type="category"
+					allowDuplicatedCategory={false}
 				/>
-				<ChartTooltip cursor={false} content={<ChartTooltipContent />} />
+				<YAxis
+					dataKey="pib"
+					// Agrega formateador para mejor visualización
+					tickFormatter={(value) => formatNumber(value)}
+				/>
+
+				<ChartTooltip
+					label="ejercicio"
+					cursor={false}
+					accessibilityLayer
+					content={
+						<ChartTooltipContent
+							labelKey="ejercicio"
+							labelFormatter={(_label, payload) => {
+								const item = payload[0];
+								if (!item) return null;
+								const {
+									payload: { ejercicio },
+								} = item;
+								return (
+									<div className="flex items-center space-x-2">
+										<Calendar className="h-5 w-5 text-muted-foreground" />
+										<h3 className="font-medium">
+											Año:{" "}
+											<span className="font-extralight text-muted-foreground">
+												{ejercicio}
+											</span>
+										</h3>
+									</div>
+								);
+							}}
+							nameKey="ejercicio"
+							indicator="dot"
+							accessibilityLayer
+							formatter={(value, name, item) => {
+								// Suponemos que payload es el objeto de datos completo con ingresos y gastos
+								const { payload } = item;
+								console.log("payload : ", payload);
+								const { ingreso, gasto } = payload;
+
+								let iconColor = "text-muted-foreground"; // Color por defecto si son iguales o no se cumple ninguna condición
+								if (ingreso > gasto) {
+									iconColor = "text-green-500";
+								} else if (gasto > ingreso) {
+									iconColor = "text-red-500";
+								}
+								return (
+									<div className="flex items-center space-x-2">
+										<BarChart2 className={`h-5 w-5 ${iconColor}`} />
+										<h3>
+											{name.toString().toUpperCase()}:{" "}
+											<span className="font-extralight text-muted-foreground">
+												{formatNumber(value)}
+											</span>
+										</h3>
+									</div>
+								);
+							}}
+						/>
+					}
+				/>
+				<Line
+					dataKey="pib"
+					type="monotone"
+					stroke="var(--color-pib)" // Define este color en tu CSS
+					strokeWidth={2}
+					dot={false}
+				/>
+
 				<Line
 					dataKey="ingreso"
 					type="monotone"
